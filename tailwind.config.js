c2bf6b6e57dc7dff335ecadbee4727567b6df6ab
Denissius laptop linux <deniskramar07@gmail.com>
2025-07-13 22:24:44 +0300

    w out redux

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx,css}",
+        "./public/index.html",
+        "./src/**/*.{js,jsx,ts,tsx,css}",
+        "./src/*.{js,jsx,ts,tsx,css}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
